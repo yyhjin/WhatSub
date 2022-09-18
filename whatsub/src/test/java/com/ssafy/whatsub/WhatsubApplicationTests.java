13c699774cac0350ae84e0f0fc01693ea95c08e8
dongwhan <ldh793@naver.com>
2022-09-18 11:33:24 +0900

    Init

---
+++ whatsub/src/test/java/com/ssafy/whatsub/WhatsubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.whatsub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
