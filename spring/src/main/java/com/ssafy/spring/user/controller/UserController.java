3d0e10f63bfae892b55e45fcb64d52a13cfe94d5
yoonhyejin <wendy0301@naver.com>
2022-09-20 16:58:15 +0900

    [#S07P22A105-97] chore: user íswagger° 세팅

--- spring/src/main/java/com/ssafy/spring/user/controller/UserController.java
+++ spring/src/main/java/com/ssafy/spring/user/controller/UserController.java
@@ -1,4 +1,16 @@
 package com.ssafy.spring.user.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(value = "user-controller", tags={"user-controller"})
+@RestController
 public class UserController {
+    @ApiOperation(value = "아이디 중복 검사", notes="판매자 아이디를 중복 검사한다. 중복이 안되면 true, 중복이면 false",httpMethod = "GET")
+    @GetMapping("/idcheck/{customer_id}")
+    public String IdCheck(String customerId) {
+        return "whatsub";
+    }
 }
