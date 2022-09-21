49f1b2bff012bc11988940b3356c84cd27644a5e
yoonhyejin <wendy0301@naver.com>
2022-09-21 15:35:06 +0900

    [#S07P22A105-106] feat: comb,recommend entity add

--- spring/src/main/java/com/ssafy/spring/comb/entity/CombinationPost.java
+++ spring/src/main/java/com/ssafy/spring/comb/entity/CombinationPost.java
@@ -1,4 +1,34 @@
 package com.ssafy.spring.comb.entity;
 
+import com.fasterxml.jackson.annotation.JsonRawValue;
+import lombok.Getter;
+import lombok.Setter;
+import springfox.documentation.spring.web.json.Json;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
 public class CombinationPost {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int combinationPostId;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "combination_id")
+    private Combination combination;
+
+    private String combName;
+
+    private String content;
+
+    private int likesCnt;
+
+    private Date createdAt;
+
+    @JsonRawValue
+    private String statistics;
 }
--- spring/src/main/java/com/ssafy/spring/recommend/entity/TodaySurvey.java
+++ spring/src/main/java/com/ssafy/spring/recommend/entity/TodaySurvey.java
@@ -1,4 +1,25 @@
 package com.ssafy.spring.recommend.entity;
 
+import com.ssafy.spring.comb.entity.Menu;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Getter
+@Setter
 public class TodaySurvey {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int todaySurveyId;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "menu_id")
+    private Menu menu;
+
+    @NotBlank
+    private String response;
 }
