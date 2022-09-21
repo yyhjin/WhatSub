13c6ea47d584b900418af8a750f48741963b8639
이현빈 <amy7852@gmail.com>
2022-09-21 14:55:32 +0900

    style: HomeView.vue 레이아웃 설정

--- vue/src/components/common/BottomNav.vue
+++ vue/src/components/common/BottomNav.vue
@@ -1,11 +1,65 @@
 <template>
-  <div></div>
+  <v-bottom-navigation :value="bottomValue - 1" color="teal" absolute>
+    <v-btn class="clickBtn bottom_width" @click="[routerPushes('home'), fetchBottomValue(1)]">
+      <span>홈</span>
+      <v-icon>mdi-home</v-icon>
+    </v-btn>
+
+    <v-btn class="clickBtn bottom_width" @click="[routerPushes(''), fetchBottomValue(2)]">
+      <span>랭킹</span>
+      <v-icon>mdi-equalizer</v-icon>
+    </v-btn>
+
+    <v-btn class="clickBtn bottom_width" @click="[routerPushes(''), fetchBottomValue(3)]">
+      <span>꿀조합</span>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+
+    <v-btn class="clickBtn bottom_width" @click="[routerPushes(''), fetchBottomValue(4)]">
+      <span>주문</span>
+      <v-icon>mdi-cart-arrow-down</v-icon>
+    </v-btn>
+
+    <v-btn class="clickBtn bottom_width" @click="[routerPushes(''), fetchBottomValue(5)]">
+      <span>프로필</span>
+      <v-icon>mdi-account</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
 </template>
 
 <script>
+import router from "@/router";
+import { mapActions, mapGetters } from "vuex";
+
 export default {
   name: "BottomNav",
+  data() {
+    return {
+      value: this.bottomValue,
+    };
+  },
+  computed: {
+    ...mapGetters(["bottomValue"]),
+  },
+  methods: {
+    ...mapActions(["fetchBottomValue"]),
+    routerPushes(icon) {
+      router.push({ name: icon });
+    },
+  },
+  watch: {
+    "$store.state.bottomValue": function () {},
+  },
 };
 </script>
 
-<style></style>
+<style>
+.v-btn.clickBtn {
+  color: #239347 !important;
+  width: 72px !important;
+}
+.bottom_width {
+  margin-left: -4px;
+  margin-right: -4px;
+}
+</style>
--- vue/src/components/home/RecommendMain.vue
+++ vue/src/components/home/RecommendMain.vue
@@ -1,13 +1,27 @@
 <template>
-  <div></div>
+  <div>
+    <h1>recommendmain</h1>
+    <div class="main_card">
+      <recommend-personal></recommend-personal>
+    </div>
+    <div class="main_card">
+      <recommend-subti></recommend-subti>
+    </div>
+  </div>
 </template>
 
 <script>
+import RecommendPersonal from "./RecommendPersonal.vue";
+import RecommendSubti from "./RecommendSubti.vue";
+
 export default {
-  name: "RecommendMain"
-}
+  name: "RecommendMain",
+  components: { RecommendPersonal, RecommendSubti },
+};
 </script>
 
 <style>
-
+.main_card {
+  margin: 20px;
+}
 </style>
