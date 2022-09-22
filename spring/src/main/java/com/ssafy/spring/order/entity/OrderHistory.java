2d398e735f2f0af38f8e20e653f7f7afdf51e63f
박현영 <qkrgusdud0925@gmail.com>
2022-09-22 17:18:32 +0900

    [#S07P22A105-95] feat: Order Entity 추가

--- spring/src/main/java/com/ssafy/spring/order/entity/OrderHistory.java
+++ spring/src/main/java/com/ssafy/spring/order/entity/OrderHistory.java
@@ -1,4 +1,18 @@
 package com.ssafy.spring.order.entity;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
 public class OrderHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int orderHistoryId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="order_id")
+    private Order order;
+
+    @NotBlank
+    private int count;
 }
