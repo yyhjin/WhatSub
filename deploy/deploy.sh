914b75c3fd3caec32a44b1c79baf0c0c79789aea
h000 <phy9250@gmail.com>
2022-09-22 11:48:31 +0900

    [#S07P22A105-98] chore: 배포 스크립트 수정

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,9 +6,9 @@ if (sudo docker ps | grep "vue"); then sudo docker stop vue; fi
 if (sudo docker ps | grep "django"); then sudo docker stop django; fi
 # if (sudo docker ps | grep "spring"); then sudo docker stop spring; fi
 
-sudo docker run -it -d --rm -p 80:80 -p 443:443 --name vue vue
+sudo docker run -it -d --rm -p 80:80 -p 443:443 --name vue whatsub/vue
 echo "Run Vue-Nginx Container"
-sudo docker run -it -d --rm -p 8000:8000  --name django django
+sudo docker run -it -d --rm -p 8000:8000  --name django whatsub/django
 echo "Run Django Container"
 # sudo docker run -it -d --rm -p 8080:8080  --name spring spring
 # echo "Run Spring Container"
