d33b6af01a35332eb8c8273a90c4dbb1ab3a2c5c
h000 <phy9250@gmail.com>
2022-09-22 10:42:50 +0900

    [#S07P22A105-98] chore: 배포 쉘 스크립트 추가

---
+++ deploy/deploy.sh
@@ -0,0 +1,14 @@
+sudo docker load < /jenkins/images_tar/vue.tar
+sudo docker load < /jenkins/images_tar/django.tar
+# sudo docker load < /jenkins/images_tar/spring.tar
+
+if (sudo docker ps | grep "vue"); then sudo docker stop vue; fi
+if (sudo docker ps | grep "django"); then sudo docker stop django; fi
+# if (sudo docker ps | grep "spring"); then sudo docker stop spring; fi
+
+sudo docker run -it -d --rm -p 80:80 -p 443:443 --name vue vue
+echo "Run Vue-Nginx Container"
+sudo docker run -it -d --rm -p 8000:8000  --name django django
+echo "Run Django Container"
+# sudo docker run -it -d --rm -p 8080:8080  --name spring spring
+# echo "Run Spring Container"
