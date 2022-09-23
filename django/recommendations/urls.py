fd0c939044010845cd168d33402820d6eae7ac07
dongwhan <ldh793@naver.com>
2022-09-23 14:46:45 +0900

    [#S07P22A105-88] feat: Content Based Filtering 초안

--- django/recommendations/urls.py
+++ django/recommendations/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.test),
+    path('api', views.api_test),
+    path('content-based-filtering', views.content_based_filtering)
 ]
--- django/recommendations/views.py
+++ django/recommendations/views.py
@@ -1,10 +1,36 @@
-from django.shortcuts import render
-from .models import User
+from django.shortcuts import render, get_list_or_404
+from .models import User, Review
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .serializers import UserSerializer
+
+from .bigdata.content_based_1 import get_content_based_filtering
 # Create your views here.
 
 def test(request):
     users = User.objects.all()
+    reviews = Review.objects.all()
     context = {
-        "users": users
+        "users": users,
+        "reviews": reviews
     }
     return render(request, 'index.html', context)
+
+@api_view(['GET'])
+def api_test(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def content_based_filtering(request): # user_id 파라미터 받기
+    # mysql에서 데이터 가져와서 content_based.py로 넘겨주기
+
+    # reviews = get_list_or_404(Review, user_id=user_id)
+    # compositions = []
+    # for review in reviews:
+    #     compositions.append(get_list_or_404(Composition, combination_id=review.combination_id))
+    result = get_content_based_filtering()
+    return Response(result)
