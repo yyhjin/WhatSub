99752c33ff17c5ef7fecc3fa0ba816517b5c3ae6
yoonkyu <newjeff@naver.com>
2022-09-27 10:02:21 +0900

    [#S07P22A105-135] style: rankview 레이아웃 초안

--- vue/src/components/rank/UserRankingItem.vue
+++ vue/src/components/rank/UserRankingItem.vue
@@ -1,13 +1,96 @@
 <template>
-  <div></div>
+  <div class="main_card">
+    <v-card class="home_card_b" elevation="3" outlined>
+      <div class="imgwrap">
+        <img :src="sand.menu_imgurl" alt="">
+      </div>
+      <div class="rank">
+        <h3 value="고기">{{ sand.menu }}</h3>
+        <h5>{{sand.rank1.ranking}}위 {{ sand.rank1.username }} {{ sand.rank1.cnt }}개</h5>
+
+      </div>
+    </v-card>
+  
+  </div>
 </template>
 
 <script>
 export default {
-  name: "UserRankingItem"
+  name: "UserRankingItem",
+
+  props:{
+    sand: Object,
+    index: Number
+  },
+
+  // data () {
+  //   return {
+  //     // ranking : this.index + 1
+  //   }
+  // },
+
+  // computed : {
+  //   ranking () {
+  //     return this.index + 1
+  //   },
+  //   name1 () {
+  //     return this.sand.rank1.username
+  //   },
+  //   name2() {
+  //     return this.sand.rank2.username
+  //   },
+  //   name3 () {
+  //     return this.sand.rank3.username
+  //   },
+  //   name4 () {
+  //     return this.sand.rank4.username
+  //   },
+  //   cnt1 () {
+  //     return this.sand.rank1.cnt
+  //   },
+  //   cnt2() {
+  //     return this.sand.rank2.cnt
+  //   },
+  //   cnt3 () {
+  //     return this.sand.rank3.cnt
+  //   },
+  //   cnt4 () {
+  //     return this.sand.rank4.cnt
+  //   }
+  // },
+
+  // mounted() {
+  //   console.log(this.sand)
+  //   console.log(this.ranking)
+  //   let h = document.querySelector('h5')
+  //   h.innerText = `${this.ranking}`
+  // }
 }
 </script>
 
-<style>
+<style scoped>
+.main_card {
+  margin: 20px;
+  width: 90%;
+  height: 80px;
+
+  
+}
+.imgwrap img {
+  display:block;
+	height:100%;
+	width:auto;
+}
+.imgwrap {
+  height: 100%;
+}
+.home_card_b {
+  height:100%;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+}
 
 </style>
--- vue/src/views/rank/RankView.vue
+++ vue/src/views/rank/RankView.vue
@@ -1,13 +1,32 @@
 <template>
-  <div></div>
+  <div>
+    <div class="top">
+      <top-nav></top-nav>
+    </div>
+    <user-ranking></user-ranking>
+  </div>
 </template>
 
 <script>
+import TopNav from '@/components/common/TopNav.vue';
+import UserRanking from '@/components/rank/UserRanking.vue';
 export default {
-  name: 'RankView'
+  name: "RankView",
+
+  components: { TopNav, UserRanking },
+
 }
+        
+ 
 </script>
 
 <style>
-
+.top {
+  position: fixed;
+  width: 100%;
+}
+.main {
+  padding-top: 56px;
+  text-align: center;
+}
 </style>
