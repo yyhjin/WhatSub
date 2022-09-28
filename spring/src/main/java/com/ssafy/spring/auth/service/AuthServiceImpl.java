a5d2855d364993d725275287a02993be7d8f773a
doyeon <dyl1229@naver.com>
2022-09-28 14:44:27 +0900

    refactor: 포트 번호 수정

--- spring/src/main/java/com/ssafy/spring/auth/service/AuthServiceImpl.java
+++ spring/src/main/java/com/ssafy/spring/auth/service/AuthServiceImpl.java
@@ -46,7 +46,7 @@ public class AuthServiceImpl implements AuthService{
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
             String sb = "grant_type=authorization_code" +
                     "&client_id=" + clientId + // REST_API_KEY 입력
-                    "&redirect_uri=http://localhost:8081/whatsub/v1/auth/login" + // 인가코드 받은 redirect_uri 입력
+                    "&redirect_uri=http://localhost:8081/api/v1/auth/login" + // 인가코드 받은 redirect_uri 입력
                     "&code=" + code;
             bw.write(sb);
             bw.flush();
