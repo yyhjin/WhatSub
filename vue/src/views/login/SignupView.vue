af4b94b0a24bb9ae9d2022857d3c2013baa71799
YoonKyu <newjeff1993@gmail.com>
2022-09-30 00:49:25 +0900

    [#] style: 회원가입 페이지 수정

--- vue/src/views/login/SignupView.vue
+++ vue/src/views/login/SignupView.vue
@@ -1,37 +1,51 @@
 <template>
-  <div>
-    <div class="title">회원가입</div>
-    <v-form class="form" @submit.prevent="signup">
-      <v-file-input prepend-icon="mdi-account"  @change="selectFile"></v-file-input>
-      <div class="nickname">
-        <v-text-field label="닉네임" v-model="credentials.username"></v-text-field>
-        <v-btn class="main_btn" @click="checkId(credentials.username)">닉네임 중복검사</v-btn>
-      </div>
-      <div class="gender">
-        성별
-        <v-radio-group v-model="credentials.gender" row class="radio">
-          <v-radio
-            label="남자"
-            value="radio-1"
-          ></v-radio>
-          <v-radio
-            label="여자"
-            value="radio-2"
-          ></v-radio>
-        </v-radio-group>
-      </div>
-      <div class="birth">
-        <v-select          
-          v-model="credentials.birthYear"
-          :items="items"
-          
-          label="출생년도"
-          data-vv-name="select"
-          required></v-select>
-      </div>
-      <v-btn class="main_btn" @click="signup">회원가입</v-btn>
-
-    </v-form>
+  <div class="body">
+    <div class="snow"></div>
+    <div class="container">
+      <div class="title">회원가입</div>
+      <v-form class="form" @submit.prevent="signup">
+        <div class="image">
+          <v-avatar>
+            <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="" id="preview">
+          </v-avatar>
+          <v-file-input
+            accept="image/png, image/jpeg, image/bmp, image/jpg"
+            placeholder="Pick an avatar"
+            prepend-icon="mdi-camera"
+            label="Avatar"
+            @change="selectFile"
+          ></v-file-input>
+        </div>
+        <div class="nickname">
+          <v-text-field label="닉네임" v-model="credentials.username"></v-text-field>
+          <v-btn class="main_btn name_btn" @click="checkId(credentials.username)">닉네임 중복검사</v-btn>
+        </div>
+        <div class="gender">
+          성별
+          <v-radio-group v-model="credentials.gender" row class="radio">
+            <v-radio
+              label="남자"
+              value="radio-1"
+            ></v-radio>
+            <v-radio
+              label="여자"
+              value="radio-2"
+            ></v-radio>
+          </v-radio-group>
+        </div>
+        <div class="birth">
+          <v-select          
+            v-model="credentials.birthYear"
+            :items="items"
+            
+            label="출생년도"
+            data-vv-name="select"
+            required></v-select>
+        </div>
+        <v-btn class="main_btn" @click="signup">회원가입</v-btn>
+      </v-form>
+    </div>
+    <div class="snow"></div>
   </div>
 </template>
 
@@ -86,10 +100,16 @@ export default {
       //formData를 axios로 서버로 보냄
       
     },
-    selectFile (event) {
-      console.log(event)
-      this.formData.append('profile_img', event)
-    },
+    selectFile (input) {
+      var reader = new FileReader();
+      reader.onload = function(e) {
+      document.getElementById('preview').src = e.target.result;
+    };
+    console.log(input)
+    console.dir(input)
+    reader.readAsDataURL(input);
+    this.formData.append('profile_img', input)
+  },
 
 
     // getToken () {
@@ -143,7 +163,27 @@ export default {
       console.error(err))
     },
 
+    createSnow () {
+      const el = document.createElement("div")
+      el.classList.add('snow')
+      el.style.position = 'relative'
+      el.style.animation = 'snow'
+      // el.style.top = 0
+      el.style.left = this.randomPosition() + 'px'
+      
+      const body = document.querySelector(".body")
+      body.appendChild(el)
+    },
+
+    randomPosition () {
+      return Math.floor(Math.random() * window.innerWidth)
+    },
+
   
+  },
+  mounted () {
+    setInterval(this.createSnow, 1000)
+
   },
 
   created() {
@@ -155,10 +195,45 @@ export default {
 
 <style scoped>
 @import "@/scss/common.scss";
-div {
+/* div {
   margin: 10px;
+} */
+.image {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 }
-
+.body {
+  height: 100vh;
+  width: 100vw;
+  /* background: radial-gradient(#f4c41f, #239347); */
+  position: relative;
+  display: flex;
+  align-items: center;
+  overflow: hidden;
+}
+.body::after {
+  content: "";
+  background: radial-gradient(#f4c41f, #239347);
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  opacity: 0.4;
+}
+.v-select{
+  padding-top: 0px;
+  margin-top: 0px;
+}
+.container {
+  height: 600px;
+  width: 80%;
+  margin: auto;
+  border: 1px solid;
+  border-radius: 20px;
+  z-index: 1;
+}
 .title {
   height:46px;
   width: 100%;
@@ -173,5 +248,50 @@ div {
 .main_btn {
   display: flex;
   justify-content: end;
+}
+.name_btn {
+  margin-left: auto;
+}
+.snow {
+  position: relative;
+  animation: snow 10s linear infinite;
+  /* left: 100px; */
+}
+.snow::after {
+  content: "";
+  height: 30px;
+  width: 30px;
+  background-image: url('@/assets/bread.png');
+  background-size: cover;
+  
+  position: absolute;
+  top: 0;
+  left: 0; 
+}
+@keyframes snow {
+  0%{
+    opacity: 0;
+    transform: translateY(-50vh);
+  }
+  20%{
+    opacity: 1;
+    transform: translate(-15px,-30vh);
+  }
+  40%{
+    opacity: 1;
+    transform: translate(15px,-10vh);
+  }
+  60%{
+    opacity: 1;
+    transform: translate(-15px,10vh);
+  }
+  80%{
+    opacity: 1;
+    transform: translate(15px,30vh);
+  }
+  100%{
+    opacity: 1;
+    transform: translateY(50vh);
+  }
 }
 </style>
