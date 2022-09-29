13599b9a0700a2a7fcec125569d94ac870121b20
YoonKyu <newjeff1993@gmail.com>
2022-09-30 01:24:16 +0900

    [#S07P22A105-174] style: ordercheckview layout(1)

--- vue/src/components/common/OrderDetail.vue
+++ vue/src/components/common/OrderDetail.vue
@@ -1,11 +1,44 @@
 <template>
-  <div></div>
+  <div>
+    <div class="store">
+      서브웨이 역삼점
+    </div>
+    <div class="orderCheck">
+      주문내역
+      <div class="menuCheck">
+
+      </div>
+      <div class="otherCheck">
+
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 export default {
   name: "OrderDetail",
+
+  computed: {
+    ...mapGetters(['selectedMenu', 'selectedBread', 'selectedCheese', 
+    'selectedSauce', 'selectedVege', 'selectedMoreMeat', 'selectedMore', 'selectedStore'])
+  }
 };
 </script>
 
-<style></style>
+<style>
+.store{
+  height: 50px;
+  width: 100%;
+  background-color: #F5F5F5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 2px;
+}
+.orderCheck{
+  width: 100%;
+  background-color: #F5F5F5;
+}
+</style>
--- vue/src/views/order/OrderCheckView.vue
+++ vue/src/views/order/OrderCheckView.vue
@@ -1,13 +1,58 @@
 <template>
-  <div></div>
+  <div class="body">
+    <div class="top">
+      주문완료!
+    </div>
+    <div class="time">
+      <p>
+        주문 번호: <span>129번</span>
+      </p>
+      <p>
+        <span>10~15분</span>정도 소요될 예정이에요.
+      </p>
+    </div>
+    <order-detail></order-detail>
+  </div>
 </template>
 
 <script>
+import OrderDetail from '../../components/common/OrderDetail.vue'
 export default {
+  components: { OrderDetail },
   name: 'OrderCheckView'
 }
 </script>
 
-<style>
-
+<style scoped>
+.body {
+  padding-top: 70px;
+}
+.top {
+  position: fixed;
+  top: 0;
+  height: 70px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #239347;
+  color: #f4c41f;
+  font-weight: bold;
+}
+.time{
+  height: 150px;
+  width: 100%;
+  border: 1px solid;   /* 나중에 지울 거 */
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  justify-content: center;
+}
+p {
+  font-size: 15px;
+}
+span {
+  font-size: 30px;
+  color: red;
+}
 </style>
