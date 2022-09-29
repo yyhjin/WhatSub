2adeca7eca0d839d7cd230522360baa44d8ca77d
이현빈 <amy7852@gmail.com>
2022-09-29 10:14:46 +0900

    [#S07P22A105-143] style: DetailTab 레이아웃 적용

--- vue/src/components/combination/detailTab/CombiDetailInfo.vue
+++ vue/src/components/combination/detailTab/CombiDetailInfo.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>CombiDetailInfo</div>
+  <div>
+    <v-row>
+      <v-col>기본메뉴</v-col>
+      <v-col>oo</v-col>
+    </v-row>
+    <v-row>
+      <v-col> 빵 </v-col>
+      <v-col> oo </v-col>
+      <v-col> 치즈 </v-col>
+      <v-col> oo </v-col>
+    </v-row>
+    <v-row>
+      <v-col> 야채 </v-col>
+      <v-col> oo </v-col>
+      <v-col> 소스 </v-col>
+      <v-col> oo </v-col>
+    </v-row>
+    <v-row>
+      <v-col> 추가재료 </v-col>
+      <v-col> oo </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
