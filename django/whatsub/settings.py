49e3b5ebff7b4d344e4d8ae087f97dcbeba8cb2b
dongwhan <ldh793@naver.com>
2022-09-30 17:37:31 +0900

    Update static root

--- django/whatsub/settings.py
+++ django/whatsub/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT =  "/var/j7a105.p.ssafy.io/static/"
+STATIC_ROOT =  BASE_DIR / 'staticfiles' / 'rest_framework'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
