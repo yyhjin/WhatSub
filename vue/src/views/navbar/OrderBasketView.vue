ccb325318e5b8c9a0debd3948e5007ce4ccaaef2
YoonKyu <newjeff1993@gmail.com>
2022-10-02 23:22:47 +0900

    [#] feat: 주문 요청 형식에 맞게 data 변환 및 axios코드 추가

--- vue/src/views/navbar/OrderBasketView.vue
+++ vue/src/views/navbar/OrderBasketView.vue
@@ -23,6 +23,8 @@
 </template>
 
 <script>
+import axios from 'axios'
+import api from "@/api/api"
 import { mapGetters } from 'vuex'
 // import OrderDetail from '../../components/common/OrderDetail.vue'
 import OrderBasket from '../../components/order/OrderBasket.vue'
@@ -43,7 +45,86 @@ export default {
 
   methods: {
     setOrder () {
-      this.$router.push('/ordercheck')
+      let combinationList = []
+      this.basket.forEach(bas => {
+        const count = bas.cnt
+        const price = bas.price
+        let combinationId = ""
+        let fat = 0
+        let kcal = 0
+        let protein = 0
+        let sodium = 0
+        let sugar = 0
+        
+        for (let value in bas) {
+          // console.log(typeof(bas[value]))
+          // console.log(Array.isArray(bas[value]))
+          if (typeof(bas[value]) === 'object' && bas[value] !== null) {
+            // console.log(bas[value])
+            if (Array.isArray(bas[value])) {
+              bas[value].forEach(each => {
+                // console.log(bas[value])
+                combinationId += each.ingredientId
+                fat += each.fat
+                kcal += each.kcal
+                protein += each.protein
+                sodium += each.sodium
+                sugar += each.sugar
+              })
+            } else {
+              if (value === 'menu') {
+                // console.log(bas[value])
+                combinationId += bas[value].menuId
+                fat += bas[value].fat
+                kcal += bas[value].kcal
+                protein += bas[value].protein
+                sodium += bas[value].sodium
+                sugar += bas[value].sugar
+              } else {
+                // console.log(bas[value])
+                combinationId += bas[value].ingredientId
+                fat += bas[value].fat
+                kcal += bas[value].kcal
+                protein += bas[value].protein
+                sodium += bas[value].sodium
+                sugar += bas[value].sugar
+              }
+            }
+          }
+        }
+      fat = Math.ceil(fat)
+      kcal = Math.ceil(kcal)
+      protein = Math.ceil(protein)
+      sodium = Math.ceil(sodium)
+      sugar = Math.ceil(sugar)
+      combinationList.push({
+        "combinationId": combinationId,
+        "count": count,
+        "fat": fat,
+        "kcal": kcal,
+        "price": price,
+        "protein": protein,
+        "sodium": sodium,
+        "sugar": sugar
+      })
+      })
+      const data = {
+        "branchId": 0,
+        "combinationList":combinationList,
+        "orderPrice":this.totalPrice,
+        "userId":1
+      }
+      axios({
+        url: api.order.order.makeorder(),
+        method: 'post',
+        data: data
+        // headers:getters.authHeader
+      }).then(res => {
+        console.log(res)
+      }).catch(err => {
+        console.error(err)
+      })
+      this.$router.push('/ordercheck') // 요청 성공하면 주소 옮기는게 나을수도
     }
   }
 }
