885669b8ad4b82f6b266fe6e1e50253ab90a9284
dongwhan <ldh793@naver.com>
2022-10-04 21:10:58 +0900

    Add staticfiles_dirs

--- django/whatsub/settings.py
+++ django/whatsub/settings.py
@@ -128,13 +128,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT =  BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
+STATIC_ROOT =  BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static',]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
