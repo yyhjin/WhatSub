70036b4043886ed18fb46e32c4009018f54b21fe
dongwhan <ldh793@naver.com>
2022-10-04 13:01:17 +0900

    django cors

--- django/whatsub/settings.py
+++ django/whatsub/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = my_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
 
 # Application definition
