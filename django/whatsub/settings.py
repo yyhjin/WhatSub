e3d788fd87f3b4e478bd6a5c99469cdc56be5720
dongwhan <ldh793@naver.com>
2022-10-04 10:49:01 +0900

    django CORS

--- django/whatsub/settings.py
+++ django/whatsub/settings.py
@@ -133,3 +133,5 @@ STATIC_ROOT =  BASE_DIR / 'staticfiles' / 'rest_framework'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
