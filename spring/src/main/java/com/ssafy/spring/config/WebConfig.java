6a8443e8c75c8d10c529dd2064e44fe03f63f51e
이도연 <dyl1229@naver.com>
2022-10-05 16:23:52 +0900

    feat: 액세스 토큰으로 유저 정보 받기 위해 resolver 추가

--- spring/src/main/java/com/ssafy/spring/config/WebConfig.java
+++ spring/src/main/java/com/ssafy/spring/config/WebConfig.java
@@ -2,13 +2,19 @@ package com.ssafy.spring.config;
 
 import com.ssafy.spring.interceptor.JWTInterceptor;
 import com.ssafy.spring.interceptor.SignupInterceptor;
+import com.ssafy.spring.resolver.ClientIpArgumentResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
@@ -16,6 +22,13 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+
+    private final ClientIpArgumentResolver clientIpArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(clientIpArgumentResolver);
     }
 
     @Autowired
@@ -27,11 +40,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // 카카오 토큰 확인(주석 해제하고 테스트 가능)
-        registry.addInterceptor(jwtInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/auth/**") //로그인 과정에 필요한 api 호출들 제외
-//                .excludePathPatterns("**/auth/**") //로그인 과정에 필요한 api 호출들 제외
-                .excludePathPatterns("/swagger-resources/**", "/swagger-ui.html/**");
+//        registry.addInterceptor(jwtInterceptor)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns("/auth/**") //로그인 과정에 필요한 api 호출들 제외
+////                .excludePathPatterns("**/auth/**") //로그인 과정에 필요한 api 호출들 제외
+//                .excludePathPatterns("/swagger-resources/**", "/swagger-ui.html/**");
 
         // 설문 가입 여부 확인(미완)
 //        registry.addInterceptor(signupInterceptor)
