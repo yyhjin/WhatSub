6ba3ea60aa9ca567594034fdf015188f365198cf
이현빈 <amy7852@gmail.com>
2022-10-07 04:32:58 +0900

    [#Issue Number] style: 랭킹 화면 style 적용 초안

--- vue/src/components/rank/UserRanking.vue
+++ vue/src/components/rank/UserRanking.vue
@@ -1,27 +1,38 @@
 <template>
   <div>
     <div class="main">
-      <h3 style="font-size: 21px">명예의 전당</h3>
+      <h3 style="font-size: 21px">
+        10월 명예의 전당
+        <span><v-icon class="mt-n2" size="40" color="amber">mdi-crown-outline</v-icon></span>
+      </h3>
     </div>
-    <user-ranking-item
-      :index="index"
-      :sand="sand"
-      v-for="(sand, index) in userRank"
-      :key="index"
-    ></user-ranking-item>
+    <div>
+      <user-ranking-item :sand="userRank"></user-ranking-item>
+    </div>
   </div>
 </template>
 
 <script>
 // import UserRankingItem from '@/components/rank/UserRanking.vue';
 import UserRankingItem from "./UserRankingItem.vue";
+// import dayjs from "dayjs";
+
 export default {
   name: "UserRanking",
 
   components: { UserRankingItem },
 
+  data() {
+    return {
+      // date: "",
+    };
+  },
+
   props: {
     userRank: Array,
+  },
+  created() {
+    // this.date = dayjs(this.userRank[0].rankDate).format("MM");
   },
 };
 </script>
