32cf657c28535c5e5d90fb0b48d42ee5ac7e0be2
yoonkyu <newjeff@naver.com>
2022-10-07 04:20:45 +0900

    [#] feat: profile 이동

--- vue/src/components/profile/CollectionList.vue
+++ vue/src/components/profile/CollectionList.vue
@@ -5,10 +5,10 @@
       style="background-color: #f4c41f; border-top: 5px solid #239347"
       align="center"
     >
-      <h4 style="color: white">My 컬렉션 ({{ myList.collections.length }})</h4>
+      <h4 style="color: white">My 컬렉션 ({{ pro.collections.length }})</h4>
     </div>
     <div class="verti_combi no-scroll">
-      <div class="collec_card" v-for="(collectionItem, index) in myList.collections" :key="index">
+      <div class="collec_card" v-for="(collectionItem, index) in pro.collections" :key="index">
         <collection-list-item :collection-item="collectionItem"></collection-list-item>
       </div>
     </div>
@@ -31,12 +31,12 @@
         <v-tab-item v-for="item in items" :key="item">
           <v-card v-if="item == '꿀조합 목록'" flat>
             <div class="pt-5 pl-6 pr-6">
-              <sand-small-list :combi-list="myList.combinations"></sand-small-list>
+              <sand-small-list :combi-list="pro.combs"></sand-small-list>
             </div>
           </v-card>
           <v-card v-else flat>
             <div class="pt-1">
-              <sand-big-list :zzim-list="myList.dibs"></sand-big-list>
+              <sand-big-list :zzim-list="pro.dibs"></sand-big-list>
             </div>
           </v-card>
         </v-tab-item>
@@ -49,7 +49,7 @@
 import CollectionListItem from "@/components/profile/CollectionListItem.vue";
 import SandSmallList from "@/components/common/mypage/SandSmallListMyCombi.vue";
 import SandBigList from "@/components/common/mypage/SandBigListMyPage.vue";
-import { mapGetters, mapActions } from "vuex";
+// import { mapGetters, mapActions } from "vuex";
 
 export default {
   name: "CollectionList",
@@ -60,27 +60,10 @@ export default {
       items: ["꿀조합 목록", "찜 목록"],
     };
   },
-  props: {},
-  computed: {
-    ...mapGetters(["myList", "profile"]),
+  props: {
+    pro:Object
   },
-  watch: {
-    "$store.state.combination.combiDetail": function () {
-      this.getMyList({
-        userName: this.profile.userName,
-      });
-    },
-  },
-  created() {
-    this.fetchProfile();
-    this.getMyList({
-      userName: this.profile.userName,
-    });
-    console.log(this.myList.combinations);
-  },
-  methods: {
-    ...mapActions(["getMyList", "fetchProfile"]),
-  },
+  
 };
 </script>
 
