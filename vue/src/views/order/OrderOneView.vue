7fdce36db9c211418569ae9b6da679e2eb63e93a
이현빈 <amy7852@gmail.com>
2022-10-06 16:16:57 +0900

    [#Issue Number] style: 주문 관련 화면 레이아웃 수정

--- vue/src/views/order/OrderOneView.vue
+++ vue/src/views/order/OrderOneView.vue
@@ -1,27 +1,31 @@
 <template>
-  <div class="body">
-    <div class="top" style="position: relative">
+  <div>
+    <div class="top">
       <top-nav></top-nav>
     </div>
-    <div>
+    <div class="pt-15">
       <div align="center" style="height: 5vh"><h3>지점 선택</h3></div>
       <search-store-map />
     </div>
-    <div style="padding: 10px">
-      <div v-if="selectedStore == null">지점을 선택해주세요</div>
-      <div v-else>
-        <v-row align="center" justify="center">
-          <v-col cols="8">
-            <h3>{{ selectedStore.branchName }}점</h3>
-            <div>{{ selectedStore.contactNo }}</div>
-            <div>{{ selectedStore.streetAddress }}</div>
-          </v-col>
-          <v-col cols="4" align="center">
-            <v-btn class="main_btn" @click="nextOrder">주문하기</v-btn>
-          </v-col>
-        </v-row>
+    <v-container>
+      <div style="padding: 10px">
+        <div v-if="selectedStore == null">지점을 선택해주세요</div>
+        <div v-else>
+          <v-row align="center" justify="center">
+            <v-col cols="8">
+              <h4>{{ selectedStore.branchName }}점</h4>
+              <div>{{ selectedStore.contactNo }}</div>
+              <div>{{ selectedStore.streetAddress }}</div>
+            </v-col>
+            <v-col cols="4" align="center">
+              <v-btn class="main_btn" @click="nextOrder">주문하기</v-btn>
+            </v-col>
+          </v-row>
+        </div>
       </div>
-    </div>
+      <br />
+      <br />
+    </v-container>
     <div class="bottom">
       <bottom-nav></bottom-nav>
     </div>
@@ -43,15 +47,11 @@ export default {
   },
 
   methods: {
-    nextOrder () {
-      this.$router.push({name:'ordertwo'})
-    }
-  }
+    nextOrder() {
+      this.$router.push({ name: "ordertwo" });
+    },
+  },
 };
 </script>
 
-<style scoped>
-.body {
-  padding-bottom: 80px;
-}
-</style>
+<style></style>
